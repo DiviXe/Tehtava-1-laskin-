c1b3e98b280e649799098cd74ae8d2d3c6f431b3
Leo Ahopalo <bgs208@myy-haaga.helia.fi>
2022-08-24 12:43:47 +0300

    laskimen toiminallisuus tehty

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [value1, setValue1] = useState();
+  const [value2, setValue2] = useState();
+  const [total, setTotal] = useState();
+
+  const add = () => {
+    const sum = +value1 + +value2;
+    setTotal(sum);
+  };
+  const subtract = () => {
+    const sum = +value1 - +value2;
+    setTotal(sum);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result = {total} </Text>
+
+      <View style={styles.Text}>
+        <TextInput
+          value={value1}
+          onChangeText={(text) => setValue1(text)}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.Text}>
+        <TextInput
+          value={value2}
+          onChangeText={(text) => setValue2(text)}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.ButtonView}>
+        <Button style={styles.Button} title="+" onPress={add} />
+        <Button style={styles.Button} title="-" onPress={subtract} />
+      </View>
     </View>
   );
 }
@@ -13,8 +44,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Text: {
+    borderBottomWidth: 2,
+    borderColor: "gray",
+    width: "50%",
+    fontWeight: "bold",
+  },
+  ButtonView: {
+    marginTop: 10,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  Button: {
+    marginLeft: 10,
+    width: "auto",
+    height: 100,
+    padding: 10,
   },
 });
